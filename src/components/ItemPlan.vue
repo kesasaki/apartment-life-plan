87d94fa89324cfd6b7e496bdcde9515d30d2a911
Kei Sasaki <sasaki.kei@nitzlab.com>
2023-02-16 13:32:08 +0900

    ページレイアウト作成

---
+++ src/components/ItemPlan.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="itemPlan">
+    <h3>{{ itemName }}</h3>
+    <ul>
+      <li><a href="#" target="_blank" rel="noopener">item1</a></li>
+      <li><a href="#" target="_blank" rel="noopener">item1</a></li>
+      <li><a href="#" target="_blank" rel="noopener">item1</a></li>
+      <li><a href="#" target="_blank" rel="noopener">item1</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'itemPlan',
+  props: {
+    itemName: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
