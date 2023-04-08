9e42843ca8a3d8ec7f18589fd8960aa5ce176c3e
Kei Sasaki <sasaki.kei@nitzlab.com>
2023-04-08 17:55:30 +0900

    説明の追加

--- src/components/ContentsHeader.vue
+++ src/components/ContentsHeader.vue
@@ -2,6 +2,14 @@
     <div class="header-logo">
       <h1>長期修繕計画シミュレーター</h1>
     </div>
+    <div class="header-explain">
+      <p>40年間の分譲マンションの長期修繕計画をシミュレーションし、
+        積立金や修繕費用が計画通りかどうかを確認します。主な用途は
+        物件購入前の資金状況の健全性確認、保有物件への長期計画の大雑把な健全性の確認です。
+        </p>
+        <p>※ 修繕項目ごとのシミュレーションは行わないため、
+          より正確な見積もりが必要な場合は別の方法を使用してください。</p>
+    </div>
 </template>
 
 <script>
@@ -19,7 +27,13 @@
 h1 {
   font-size: 36px;
   font-weight: 700;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+}
+
+.header-explain {
+  max-width: 1000px;
+  margin: 10px auto;
+  color: #333;
 }
 
 </style>
